ce559c2c8b668bda0d04b1553a0e59ace7f982fb
Adam Rosien <adam@rosien.net>
2015-02-11 17:00:19 -0800

    fixes

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -3,6 +3,23 @@ package net.rosien
 package object ocscala {
   import org.scalacheck._
 
+  val diamond =
+    """|
+       |     A
+       |    B B
+       |   C   C
+       |  D     D
+       | E       E
+       |F         F
+       | E       E
+       |  D     D
+       |   C   C
+       |    B B
+       |     A
+    """.stripMargin
+
+  ///////////
+
   val propConcatLists =
     Prop.forAll { (l1: List[Int], l2: List[Int]) =>
       l1.size + l2.size == (l1 ::: l2).size
@@ -15,6 +32,28 @@ package object ocscala {
 
   ///////////
 
+  object StringSpecification extends Properties("String") {
+    import Prop.forAll
+
+    property("startsWith") = forAll { (a: String, b: String) =>
+      (a+b).startsWith(a)
+    }
+
+    property("endsWith") = forAll { (a: String, b: String) =>
+      (a+b).endsWith(b)
+    }
+
+    property("substring") = forAll { (a: String, b: String) =>
+      (a+b).substring(a.length) == b
+    }
+
+    property("substring") = forAll { (a: String, b: String, c: String) =>
+      (a+b+c).substring(a.length, a.length+b.length) == b
+    }
+  }
+
+  ///////////
+
   import shapeless.contrib.scalacheck._
 
   case class Nerb(i: Int, s: String)
@@ -23,24 +62,4 @@ package object ocscala {
     Prop.forAll { (nerb: Nerb) =>
       true
     }
-}
-
-object StringSpecification extends Properties("String") {
-  import Prop.forAll
-
-  property("startsWith") = forAll { (a: String, b: String) =>
-    (a+b).startsWith(a)
-  }
-
-  property("endsWith") = forAll { (a: String, b: String) =>
-    (a+b).endsWith(b)
-  }
-
-  property("substring") = forAll { (a: String, b: String) =>
-    (a+b).substring(a.length) == b
-  }
-
-  property("substring") = forAll { (a: String, b: String, c: String) =>
-    (a+b+c).substring(a.length, a.length+b.length) == b
-  }
 }
